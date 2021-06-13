920d29379e76280ad05c9b9cab732ed9497d25e7
Damian Karczmarz <damian.karczmarz92@gmail.com>
2021-06-13 18:00:39 +0200

    MultiplyController part2

--- src/main/java/pl/coderslab/controller/views/MultiplyController.java
+++ src/main/java/pl/coderslab/controller/views/MultiplyController.java
@@ -3,6 +3,7 @@ package pl.coderslab.controller.views;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,5 +13,11 @@ public class MultiplyController {
     public String multiply(@RequestParam(defaultValue = "10") int size, Model m){
         m.addAttribute("size",size);
         return "multiply";
+    }
+    @GetMapping("/paramMultiply/{rows}/{cols}")
+    public String paramMultiply(@PathVariable int rows,@PathVariable int cols, Model m){
+        m.addAttribute("rows",rows);
+        m.addAttribute("cols",cols);
+        return "multiply2";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/multiply2.jsp
@@ -0,0 +1,27 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: damian
+  Date: 13.06.2021
+  Time: 15:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<html>
+<head>
+    <title>Multiply</title>
+</head>
+<body>
+<h4>Tabliczka mnożenia</h4>
+<table border="1">
+    <c:forEach begin="1" end="${rows}" var="i">
+        <tr>
+            <c:forEach begin="1" end="${cols}" var="j">
+                <td>${i*j}</td>
+            </c:forEach>
+        </tr>
+    </c:forEach>
+</table>
+</body>
+</html>
